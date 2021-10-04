dd0490b4704f3fddc77812aea0b9e87b2f77b688
Congyu <52687642+Congyuwang@users.noreply.github.com>
2021-10-04 08:24:00 +0800

    Update error.rs

--- src/database/error.rs
+++ src/database/error.rs
@@ -26,7 +26,7 @@ impl Error {
         use std::str::from_utf8;
         use std::ffi::CStr;
 
-        let err_string = from_utf8(CStr::from_ptr(message).to_bytes()).unwrap().to_string();
+        let err_string = from_utf8(CStr::from_ptr(message).to_bytes()).unwrap_or("Unknown Error").to_string();
         leveldb_free(message as *mut c_void);
         Error::new(err_string)
     }
